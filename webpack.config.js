cf91022e9a76566bc55cf35d1ad43aca00905c66
Felipe Leão <felipe@felipeleao.com>
2019-02-13 15:58:12 -0200

    Add Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = ({ mode = 'development' }) => ({
+  mode,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 8080
+  }
+})
